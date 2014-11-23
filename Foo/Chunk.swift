aed99022e195f5780ed654909adf99ebbf7e1f64
David Paschich <dpassage@balveda.com>
2014-11-23 15:23:37 -0800

    Tried adding Chunk.swift to replace Chunk

---
+++ Foo/Chunk.swift
@@ -0,0 +1,13 @@
+//
+//  Chunk.swift
+//  Foo
+//
+//  Created by David Paschich on 11/23/14.
+//  Copyright (c) 2014 David Paschich. All rights reserved.
+//
+
+import Foundation
+
+@objc class Chunk {
+    
+}
